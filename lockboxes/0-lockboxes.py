9647b230319687aad67bc85a6c32ee74f7cbfe50
Hadrien <144351263+Iaskasan@users.noreply.github.com>
2025-12-11 14:51:05 +0100

    added lockboxes task

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """mindbending game of lockboxes"""
+    keys = set([0] + boxes[0])
+    opened = set([0])
+    for j in range(len(boxes)):
+        if j in keys:
+            opened.add(j)
+        for i in range(len(boxes)):
+            if i in keys:
+                keys.update(boxes[i])
+    return len(opened) == len(boxes)
