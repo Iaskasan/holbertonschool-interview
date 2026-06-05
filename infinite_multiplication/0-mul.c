4014b6df7c7e9828620c5d81f7e092f5fa1cecde
Iaskasan <nokiesan@yahoo.fr>
2026-06-05 09:48:47 +0200

    changed the int type to handle big numbers

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 
 int _putchar(char c);
 
@@ -16,26 +17,76 @@ void print_error_and_exit(void)
 }
 
 /**
- * print_ull - print an unsigned long long using _putchar
- * @n: number to print
+ * multiply_strings - multiplies two number strings
+ * @num1: first number as string
+ * @num2: second number as string
+ * Return: result as allocated string
  */
-void print_ull(unsigned long long n)
+char *multiply_strings(char *num1, char *num2)
 {
-	char buf[32];
-	int i = 0;
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int result_len = len1 + len2;
+	int *result = malloc(result_len * sizeof(int));
+	int i, j, mul, p, carry;
+	char *str;
 
-	if (n == 0)
+	if (!result)
+		return (NULL);
+
+	/* Initialize result array */
+	for (i = 0; i < result_len; i++)
+		result[i] = 0;
+
+	/* Multiply each digit */
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		_putchar('0');
-		return;
+		carry = 0;
+		mul = num1[i] - '0';
+		p = i + len2;
+
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int product = mul * (num2[j] - '0') + result[p] + carry;
+
+			result[p] = product % 10;
+			carry = product / 10;
+			p--;
+		}
+
+		if (carry > 0)
+			result[p] += carry;
 	}
-	while (n > 0)
+
+	/* Find start of result (skip leading zeros) */
+	i = 0;
+	while (i < result_len - 1 && result[i] == 0)
+		i++;
+
+	/* Allocate and build result string */
+	str = malloc((result_len - i + 1) * sizeof(char));
+	if (!str)
 	{
-		buf[i++] = '0' + (n % 10);
-		n /= 10;
+		free(result);
+		return (NULL);
 	}
-	while (i--)
-		_putchar(buf[i]);
+
+	for (j = 0; i < result_len; i++, j++)
+		str[j] = result[i] + '0';
+	str[j] = '\0';
+
+	free(result);
+	return (str);
+}
+
+/**
+ * print_string - print a string using _putchar
+ * @s: string to print
+ */
+void print_string(char *s)
+{
+	while (*s)
+		_putchar(*s++);
 }
 
 /**
@@ -47,7 +98,7 @@ void print_ull(unsigned long long n)
 int main(int argc, char *argv[])
 {
 	char *p;
-	unsigned long long n1, n2;
+	char *result;
 
 	if (argc != 3)
 	{
@@ -70,13 +121,16 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	/* Convert strings to unsigned long long */
-	n1 = strtoull(argv[1], NULL, 10);
-	n2 = strtoull(argv[2], NULL, 10);
+	/* Multiply the strings */
+	result = multiply_strings(argv[1], argv[2]);
+
+	if (!result)
+		print_error_and_exit();
 
 	/* Print the result */
-	print_ull(n1 * n2);
+	print_string(result);
 	_putchar('\n');
 
+	free(result);
 	return (0);
 }
